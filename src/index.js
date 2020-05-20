eda6677e1514779a8e1ee67de37b76d1108041c6
kondratievm <mkwtzt@gmail.com>
2020-05-20 20:39:02 +0400

    react basic

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+let welcome = <h1>HUI</h1>;
+let target = document.getElementById('root');
 
-function Hello() {
-   return (
-      <div>
-         <h1>Hello</h1>
-      </div>
-   )
-}
+const user = {
+   name: 'Petr Petrov',
+   age: 40,
+   hobby: [
+      'codding',
+      'music',
+      'sport'
+   ]
+};
 
+const userInfoList = (
+   <ul className='info-list'>
+      <li>FIO: {user.name}</li>
+      <li>Age: {user.age}</li>
+      <li>Hobby: {user.hobby}</li>
+   </ul>
+)
 
-render(<Hello/>, document.getElementById('root'));
+render(userInfoList, target);
