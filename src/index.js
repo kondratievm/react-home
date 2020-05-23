ab7ae2358af30182ca505850d83087f76508564b
kondratievm <mkwtzt@gmail.com>
2020-05-23 19:31:17 +0400

    conditions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,70 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-let welcome = <h1>HUI</h1>;
-let target = document.getElementById('root');
+const target = document.getElementById('root');
 
-const user = {
-   name: 'Petr Petrov',
-   age: 40,
-   hobby: [
-      'codding',
-      'music',
-      'sport'
-   ]
-};
+// const user = {
+//    name: 'Petrov Petr',
+//    age: 30,
+//    hobby: ['codding', 'music', 'sport']
+// };
 
-const userInfoList = (
-   <ul className='info-list'>
-      <li>FIO: {user.name}</li>
-      <li>Age: {user.age}</li>
-      <li>Hobby: {user.hobby}</li>
-   </ul>
-)
+// const UserInfo = function(props) {
+//    const {name, age, hobby} = props;
 
-render(userInfoList, target);
+//    return (
+//       <ul className='info-list'>
+//          <li>FIO: {name}</li>
+//          <li>Age: {age}</li>
+//          <li>Hobby: {hobby.join(', ')}</li>
+//       </ul>
+//    )
+// }
+
+// const app = (
+//    <main>
+//       <UserInfo name={user.name} age={user.age} hobby={user.hobby}/>
+//    </main>
+// );
+
+// const HelloComponent = props => {
+//    let {name} = props;
+
+//    return (
+//       <h1>Hi, {name}!</h1>
+//    )
+// }
+
+// const app = (
+//    <main>
+//       <HelloComponent name='Maxim'/>
+//    </main>
+// );
+
+const todoList = [
+   'купить молоко',
+   'прочитать статью',
+   'купить билеты'
+];
+
+function List({ list, title}) {
+   if (!list) {
+      return null;
+   }
+   if (!list.length) {
+      return <p>Список пуст</p>
+   }
+
+   const items = list.map((item, index) => <li key={index}>{item}</li>);
+
+   return (
+      <div>
+         <h1>{title}</h1>
+         <ul>
+            { items }
+         </ul>
+      </div>
+   )
+}
+
+render(<List list={todoList} title='Список'/>, target);
