5eaaa82fb18457c189c5644b0daaf84f28ebdc4d
kondratievm <mkwtzt@gmail.com>
2020-06-10 21:03:35 +0400

    menu done

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,31 +1,62 @@
 import React from 'react';
+import './menu.scss';
 
 export const menuItems = [
-   { title: 'Main', href: '#home' },
-   { title: 'About', href: '#about' },
-   { title: 'Contacts', href: '#contacts' }
+   { title: 'Главная страница', href: '#home' },
+   { title: 'О компании', href: '#about' },
+   { title: 'Контакты', href: '#contacts' }
 ];
 
-export const ModalMenu = function (props) {
-   const isOpen = props.isOpen;
-   const listItems = menuItems.map((listItem) => <li><a href={listItem.href}>{listItem.title}</a></li>);
-
-   if (isOpen) {
+function CloseBtn(props) {
+   const isClosed = props.isClosed;
+   if (isClosed) {
       return (
-         <div className="menu menu-open">
-            <div className="menu-toggle"><span></span></div>
-            <nav>
-               <ul>
-                  {listItems}
-               </ul>
-            </nav>
+         <div>
+            <input id="menu__toggle" type="checkbox" checked/>
+            <label className="menu__btn" for="menu__toggle">
+               <span></span>
+            </label>
          </div>
       )
    }
    return (
-      <div className="menu">
-         <div className="menu-toggle"><span></span></div>
+      <div>
+         <input id="menu__toggle" type="checkbox" />
+         <label className="menu__btn" for="menu__toggle">
+            <span></span>
+         </label>
       </div>
    )
 }
 
+export const ModalMenu = function (props) {
+   const isOpen = props.isOpen;
+   const listItems = menuItems.map((listItem, index) =>
+      <li key={index}>
+         <a href={listItem.href}>{listItem.title}</a>
+      </li>);
+
+   if (isOpen) {
+      return (
+         <div>
+            <CloseBtn isClosed={true} />
+            <div className="menu menu-open">
+               <div className="menu-toggle"><span></span></div>
+               <nav>
+                  <ul>
+                     {listItems}
+                  </ul>
+               </nav>
+            </div>
+         </div>
+      )
+   }
+   return (
+      <div>
+         <CloseBtn isClosed={false} />
+         <div className="menu menu-closed">
+            <div className="menu-toggle"><span></span></div>
+         </div>
+      </div>
+   )
+}
